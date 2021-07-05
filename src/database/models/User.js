2b9cda1701a6bc4ee265d72b794ce862f8ea8a22
Pedro <pedrotzf@gmail.com>
2021-07-05 20:13:06 -0300

    feat(User.js): model de usuário

---
+++ src/database/models/User.js
@@ -0,0 +1,36 @@
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const sequelize = new Sequelize();
+
+class User extends Model {}
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    updated_at: {
+        type: DataTypes.DATE
+    }
+}, {
+    sequelize,
+    modelName: 'users'
+})
