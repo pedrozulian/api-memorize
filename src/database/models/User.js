b2a1a8ad1c8083caf688617b9de99e1051b40f57
Pedro <pedrotzf@gmail.com>
2021-07-05 21:23:47 -0300

    feat(User.js): migration e model de usuarios

--- src/database/models/User.js
+++ src/database/models/User.js
@@ -22,13 +22,6 @@ User.init({
     password: {
         type: DataTypes.STRING,
         allowNull: false
-    },
-    created_at: {
-        type: DataTypes.DATE,
-        allowNull: false
-    },
-    updated_at: {
-        type: DataTypes.DATE
     }
 }, {
     sequelize,
