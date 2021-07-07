0473620eacf864e6825834aa8bdea9e7829039a7
Pedro <pedrotzf@gmail.com>
2021-07-06 21:02:32 -0300

    feat(model/migration): de itens da pasta (lista de flashcards) é um item

---
+++ src/database/migrations/20210706235842-create-items-folder-list-flashcards.js
@@ -0,0 +1,21 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('items_folder_list_flashcards', {
+      id_folder: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: 'folder_list_flashcards', key: 'id' }
+      },
+      id_list: {
+        type: {
+          type: Sequelize.INTEGER,
+          references: { model: 'list_flashcards', key: 'id' }
+        }
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('items_folder_list_flashcards');
+  }
+};
