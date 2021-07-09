44c2f19ab3df141d8a9b6d4b012b27e5f3cf0154
Pedro <pedrotzf@gmail.com>
2021-07-09 15:42:47 -0300

    feat(user.routes.js): rota p buscar usuario

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,4 +3,6 @@ const UserController = require('../controllers/user.controller');
 
 router.post('/', UserController.create);
 
+router.get('/:idUser', UserController.getById);
+
 module.exports = router;
