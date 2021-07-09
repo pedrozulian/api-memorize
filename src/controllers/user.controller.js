8eb60ab69f36ea413182b6607996d84920ab56a4
Pedro <pedrotzf@gmail.com>
2021-07-09 15:42:33 -0300

    feat(user.controller.js): buscando usuario pelo id

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,15 +1,24 @@
 const { User } = require('../models');
 
 class UserController {
-    async create (req, res) {
+    async create(req, res) {
         try {
             const user = await User.create(req.body);
-            return res.status(200).send({ message: 'User created' });
+            return res.status(201).send({ message: 'User created' });
         } catch (error) {
             if (error.name == 'SequelizeUniqueConstraintError') {
                 return res.status(400).send({ message: 'E-mail already used' });
             }
-            return res.status(500).send(error);
+            return res.status(500).send({ error: error.message });
+        }
+    }
+
+    async getById(req, res) {
+        try {
+            const user = await User.findOne({ where: { id: Number(req.params.idUser) }});
+            return res.status(200).send(user);
+        } catch (error) {
+            return res.status(500).send({ error: error.message });
         }
     }
 }
