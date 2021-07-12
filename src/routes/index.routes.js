4a8ff9d164fbac741e80513644c38532ef0f1fa0
Pedro <pedrotzf@gmail.com>
2021-07-12 18:41:30 -0300

    feat(index.routes.js): importando rotas de sessão

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
 router.use('/users', require('./user.routes'));
+router.use('/session', require('../routes/session.routes'));
 
 module.exports = router;
